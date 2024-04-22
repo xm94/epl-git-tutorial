c213643e7e72fe9fd2904ffcf8512d22eec5f3b4
HanabiKudo <retro.mendozaa@gmail.com>
2024-04-22 14:38:48 -0600

    added charecter class file and a log in index

---
+++ hana.ts
@@ -0,0 +1,20 @@
+export class character{
+    name: string;
+    level: number;
+    job: string;
+    
+
+    constructor(
+        username: string,
+        level: number,
+        job: string,
+    
+    ){
+        this.name = username
+        this.level = level
+        this.job = job
+    
+    };
+}
+
+
