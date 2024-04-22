4489aca2e266cc228150ad85ff5395daaef5ee55
SushiRoll53 <bryan@elpasolabs.com>
2024-04-22 14:30:08 -0600

    Add Bryan developer

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import { Developer } from "./dev";
 
 console.log("Hello world!");
 
-let xavier = new Developer("Xavier","Typescript","Backend");
+let xavier = new Developer("Xavier", "Typescript", "Backend");
+
+let bryan = new Developer("Bryan", "Typescript", "Backend");
 
 console.log(xavier);
+
+console.log(bryan);
