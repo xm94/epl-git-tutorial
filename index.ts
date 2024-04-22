647f04466447db88c6fc5ac266a52dcac76a8701
SushiRoll53 <bryan@pif.co>
2024-04-22 14:47:55 -0600

    Log Bryan dev

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import { character } from "./hana";
 
 console.log("Hello world!");
 
-let xavier = new Developer("Xavier",["Typescript","Java","Dart"],"Backend");
+let xavier = new Developer("Xavier", ["Typescript", "Java", "Dart"], "Backend");
+
+let bryan = new Developer("Bryan", ["Dart", "Java"], "Backend");
 
 console.log(xavier);
 console.log(character);
+console.log(bryan);
 
